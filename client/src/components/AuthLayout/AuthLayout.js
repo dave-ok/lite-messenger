a018ecbb3b8f43ba05e1a2ec094039042419e32a
Dave <callremz@gmail.com>
2021-04-26 05:56:39 +0100

    setup auth layout

---
+++ client/src/components/AuthLayout/AuthLayout.js
@@ -0,0 +1,28 @@
+import { Box, Grid, makeStyles } from "@material-ui/core";
+import React from "react";
+import { AuthSidebar } from ".";
+
+const useStyles = makeStyles({
+  root: {
+    height: "100vh",
+    fontFamily: "Open Sans",
+  },
+});
+const AuthLayout = ({ Form, Nav }) => {
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.root}>
+      <Grid item xs={12} sm={6} md={4}>
+        <AuthSidebar />
+      </Grid>
+      <Grid item xs={12} sm={6} md={8}>
+        <Box p={4}>
+          {Nav}
+          {Form}
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default AuthLayout;
