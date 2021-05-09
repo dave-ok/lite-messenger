32f57bdf2ff86c054e62d0b9b1344c21073b724d
Dave <callremz@gmail.com>
2021-05-09 15:45:20 +0100

    init test setup and teardown

---
+++ server/test/setupAndTeardown.js
@@ -0,0 +1,38 @@
+const { expect } = require("chai");
+const { Op } = require("sequelize");
+const { User } = require("../db/models");
+
+const testUsers = [
+  { username: "user1", password: "password1", email: "email1@xyz.com" },
+  { username: "user2", password: "password2", email: "email2@xyz.com" },
+  { username: "user3", password: "password3", email: "email3@xyz.com" },
+];
+
+let users;
+
+before(async () => {
+  // create test users
+  try {
+    users = await User.bulkCreate(testUsers, {
+      validation: true,
+      returning: true,
+    });
+  } catch (error) {
+    throw error;
+  }
+
+  expect(users).lengthOf(3);
+});
+
+after(async () => {
+  if (users) {
+    userIds = users.map((user) => user.id);
+    await User.destroy({
+      where: {
+        id: {
+          [Op.in]: userIds,
+        },
+      },
+    });
+  }
+});
